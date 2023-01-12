eda74c575860c9ba6d174b96950352bb65c105ca
mjconl6291 <mjconl6291@gmail.com>
2023-01-12 01:47:10 -0600

    context

--- src/app.py
+++ src/app.py
@@ -7,8 +7,8 @@ import boto3
 import os
 
 def handler(event,context):
-    queue_id = event['queue_id']
-    patient_id = event['patient_id']
+    queue_id = context['queue_id']
+    patient_id = context['patient_id']
     ssm = boto3.client('ssm',  aws_access_key_id=os.environ['KEY'], aws_secret_access_key=os.environ['SECRET'],  region_name='us-east-2')
     param = ssm.get_parameter(Name='uck-etl-db-prod-masterdata', WithDecryption=True )
     db_request = json.loads(param['Parameter']['Value']) 
